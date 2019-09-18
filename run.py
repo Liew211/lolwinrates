d2f19130b8fef7a84d7b4ba133a480c03fc34c0f
liew211 <samliew211@outlook.com>
2019-09-18 18:29:32 -0400

    Add input validation

--- run.py
+++ run.py
@@ -1,7 +1,24 @@
 import api_request as api
 
+# Switch statement for queue type
+def switch(queue):
+    switcher = {
+        'blind': 430,
+        'draft': 400,
+        'solo': 420,
+        'flex': 440
+    }
+    return switcher.get(queue, 0)
+
+# Prompt user for inputs
 print("Summoner Name?")
 summonerId = input()
-print("Queue code?")
-queue = int(input())
-api.displayWinrates(api.getMatchList(api.getSummonerId(summonerId),queue))
+
+queueCode = 0
+# Verify queue input validation
+while queueCode == 0:
+    print("Which queue?")
+    queue = input()
+    queueCode = switch(queue)
+
+api.displayWinrates(api.getMatchList(api.getSummonerId(summonerId),queueCode))
