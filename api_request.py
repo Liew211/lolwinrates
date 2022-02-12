ed7d95b21a00545d017b25d1d5874aa5a0444c04
Sam Liew <samliew211@outlook.com>
2022-02-12 00:36:54 -0500

    Update docs

--- api_request.py
+++ api_request.py
@@ -15,61 +15,39 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Safari/605.1.15"
 }
 
-# get latest version
-version = requests.get(
-    'https://ddragon.leagueoflegends.com/api/versions.json').json()[0]
 
-# convert key to champion name
-datadragon = requests.get(
-    f'http://ddragon.leagueoflegends.com/cdn/{version}/data/en_US/champion.json').json()
-champions = {}
-for champion in datadragon["data"]:
-    key = int(datadragon["data"][champion]["key"])
-    champions[key] = champion
-
-
-def championId_to_name(id: int):
-    return champions[id]
-
-# Given a list of wins and losses, calculate the winrate as a percentage
-
-
-def winrate(winloss: list):
-    return winloss[0] / (winloss[0] + winloss[1]) * 100
-
-# Each player has an encrypted account ID that is used to get match history and other data
+def winrate(wins, losses):
+    """
+    Given number of wins and losses, calculate the winrate as a percentage
+    """
+    return wins / (wins + losses) * 100
 
 
 def getSummonerId(summonerName: str, region):
+    """
+    Each player has an encrypted account ID that is used to get match history and other data
+    """
     summonerNameUrl = f'https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}'
-    response = requests.get(summonerNameUrl, headers=headers).json()
-    if response['status']['status_code'] != 200:
-        print(response['status']['message'])
+    response = requests.get(summonerNameUrl, headers=headers)
+    if not response.ok:
+        print(response.json()['status']['message'])
         exit()
-    return response['puuid']
-
-
-# List of past matches from the player in a certain queue
-# Queue codes:
-# 400 - 5v5 Draft Pick
-# 420 - 5v5 Ranked Solo
-# 430 - 5v5 Blind Pick
-# 440 - 5v5 Ranked Flex
-# 450 - 5v5 ARAM
+    return response.json()['puuid']
 
 
 def getMatchList(summonerId: str, queue: int, routing):
-    params = f'?queue={queue}&count=50'
+    params = f'?queue={queue}&count=5'
     summonerMatchlistUrl = f'https://{routing}.api.riotgames.com/lol/match/v5/matches/by-puuid/{summonerId}/ids{params}'
     res = requests.get(summonerMatchlistUrl, headers=headers).json()
     return res
 
-# Given matchlist, print the overall winrate and the list of champions played, their winrates, and number of games played
-# Also returns the champion names and their respective winrates as two lists
-
 
 def displayWinrates(summonerId, matchList: list, routing):
-    win_loss = [0, 0]
+    """
+    Iterates through matchlist, prints out results and returns list of
+    champions played, and a list of their corresponding winrates
+    """
+    wins, losses = (0, 0)
     champion_winrates = {}
 
     # Parse through matchlist
@@ -87,13 +65,13 @@ def displayWinrates(summonerId, matchList: list, routing):
 
         # Increments win/loss counters for overall and per champion
         if win:
-            win_loss[0] += 1
+            wins += 1
             if champion in champion_winrates:
                 champion_winrates[champion][0] += 1
             else:
                 champion_winrates[champion] = [1, 0, 0]
         else:
-            win_loss[1] += 1
+            losses += 1
             if champion in champion_winrates:
                 champion_winrates[champion][1] += 1
             else:
@@ -107,10 +85,10 @@ def displayWinrates(summonerId, matchList: list, routing):
     champion_winrates_list = []
 
     # Overall wins and losses
-    print(win_loss[0], 'wins', win_loss[1], 'losses')
+    print(wins, 'wins', losses, 'losses')
 
     # Overall winrate to two decimal places
-    print('%.2f' % (winrate(win_loss)) + "%")
+    print('%.2f' % (winrate(wins, losses)) + "%")
     for champion in champion_winrates:
         # Prints champion winrates and checks for plural games
         percentage = '%.2f' % (winrate(champion_winrates[champion]))
