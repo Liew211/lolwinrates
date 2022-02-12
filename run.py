0ac133c8c15b9d6e1fec48af8782c96f75fef362
Christopher Frige <59812260+christopherfrige@users.noreply.github.com>
2022-02-11 23:09:48 -0300

    Added some style in the graph, console description and input validation (#1)

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import api_request as api
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 plt.style.use('ggplot')
 
 # Switch statement for queue type
@@ -15,7 +16,27 @@ def switch(queue):
     }
     return switcher.get(queue, 0)
 
+def platformSelector(region):
+    regions = {
+        "br": "br1",
+        "eune": "eun1",
+        "euw": "euw1",
+        "jp": "jp1",
+        "kr": "kr",
+        "lan": "la1",
+        "las": "la2",
+        "na": "na1",
+        "oce": "oc1",
+        "tr": "tr1",
+        "ru": "ru"
+    }
+    return regions.get(region, 0)
+
 # Prompt user for inputs
+# Region selector
+region = input("Which Region? (i.e. NA)\n").lower()
+region = platformSelector(region)
+
 # Does not verify input for summoner name
 print("Summoner Name?")
 summonerId = input()
@@ -27,14 +48,37 @@ while queueCode < 0:
     queue = input().lower()
     queueCode = switch(queue)
 
+print("Loading information from the nickname...")
+
+# Gets start and end time for the function
+start = time.time()
 # Gets list of champions & their winrates as a pair of lists, in order of number of games played
-x, y = api.displayWinrates(api.getMatchList(api.getSummonerId(summonerId),queueCode))
+try:
+    x, y = api.displayWinrates(api.getMatchList(api.getSummonerId(summonerId, region),queueCode, region), region)
+# Exception for when the summonerId request gives an error 
+except:
+    print("That summoner name doesn't exist!")
+    exit()
+
+end = time.time()
+print(f"Data acquired in {end-start:.2f} seconds.")
 
 fig, ax = plt.subplots()
 
 # Plots bar graph
 x_pos = np.arange(len(x))
 bar_plot = plt.bar(x_pos, y)
+
+# Set bar colours for different winrates
+for i, bar in enumerate(bar_plot):
+    if y[i]>=70:
+        bar.set_color("#FFAB0F")
+    elif y[i]>=60 and y[i]<70:
+        bar.set_color("#006AB7")
+    elif y[i]>=50 and y[i]<60:
+        bar.set_color("#80C1F0")
+    else:
+        bar.set_color("grey")
 
 # Creates labels for each bar
 for champion,rect in enumerate(bar_plot):
