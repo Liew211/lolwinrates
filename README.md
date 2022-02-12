43e9dec86ac07f295945d38514a92ad1731240c5
Sam Liew <samliew211@outlook.com>
2022-02-11 23:39:56 -0500

    Update API endpoints

--- README.md
+++ README.md
@@ -2,26 +2,28 @@
 
 [![Python 3.7.4](https://img.shields.io/badge/python-3.7.4-blue.svg)](https://www.python.org/downloads/release/python-374/)
 
-**lolwinrates** calculates and displays your winrate in League of Legends games by using Python with Matplotlib to access the [Riot Games API](https://developer.riotgames.com).  The API can be accessed by signing in with your League of Legends account, where you can get your 24-hour developer API key.
+**lolwinrates** calculates and displays your winrate in League of Legends games by using Python with Matplotlib to access the [Riot Games API](https://developer.riotgames.com). The API can be accessed by signing in with your League of Legends account, where you can get your 24-hour developer API key.
 
-To use this program, replace the API key on line 4 of `api_request.py`, then run `run.py`.  You'll be prompted to enter the summoner name (not case sensitive), and the queue type: "blind", "draft", "solo", or "flex" (also not case sensitive).  The script will access your match history, and parse through up to 50 of your past games in that queue, then display your win-loss record and win percentage, then the list of champions you played, sorted by frequency, the winrates of each champion, and number of games played for each champion, and creates a bar graph with champion winrates using Matplotlib.
+To use this program, replace the API key on line 2 of `config.cfg`, then run `run.py`. You'll be prompted to enter the region code, summoner name, and the queue type: "blind", "draft", "solo", or "flex". The script will access your match history, and parse through up to 50 of your past games in that queue, then display your win-loss record and win percentage, then the list of champions you played, sorted by frequency, the winrates of each champion, and number of games played for each champion, and creates a bar graph with champion winrates using Matplotlib.
 
 Sample output:
 ```
+Region? (i.e. NA)
+NA
 Summoner Name?
-Liew211
+liew211
 Which queue?
-Solo
-29 wins 21 losses
-58.00%
-Sivir 72.00% 25 games
-Jinx 46.15% 13 games
-Ezreal 60.00% 5 games
-Fiora 0.00% 2 games
-Caitlyn 0.00% 1 game
-Ashe 100.00% 1 game
-Vladimir 0.00% 1 game
-Corki 0.00% 1 game
-Garen 100.00% 1 game
+solo
+Loading...
+26 wins 24 losses
+52.00%
+Jinx 62.50% 16 games
+Vayne 33.33% 12 games
+Jhin 50.00% 10 games
+Ezreal 50.00% 8 games
+Senna 100.00% 2 games
+Nautilus 100.00% 1 game
+Tristana 0.00% 1 game
+Data acquired in 22.95 seconds.
 ```
 ![](sample.png)
--- run.py
+++ run.py
@@ -2,114 +2,132 @@ import api_request as api
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+
+
 plt.style.use('ggplot')
 
-# Switch statement for queue type
-def switch(queue):
-    switcher = {
-        'custom': 0,
-        'blind': 430,
-        'draft': 400,
-        'solo': 420,
-        'flex': 440,
-        'aram': 450
-    }
-    return switcher.get(queue, 0)
+REGIONS = {
+    "br": "br1",
+    "eune": "eun1",
+    "euw": "euw1",
+    "jp": "jp1",
+    "kr": "kr",
+    "lan": "la1",
+    "las": "la2",
+    "na": "na1",
+    "oce": "oc1",
+    "tr": "tr1",
+    "ru": "ru"
+}
 
-def platformSelector(region):
-    regions = {
-        "br": "br1",
-        "eune": "eun1",
-        "euw": "euw1",
-        "jp": "jp1",
-        "kr": "kr",
-        "lan": "la1",
-        "las": "la2",
-        "na": "na1",
-        "oce": "oc1",
-        "tr": "tr1",
-        "ru": "ru"
-    }
-    return regions.get(region, 0)
+QUEUES = {
+    'blind': 430,
+    'draft': 400,
+    'solo': 420,
+    'flex': 440,
+    'aram': 450
+}
 
-# Prompt user for inputs
-# Region selector
-region = input("Which Region? (i.e. NA)\n").lower()
-region = platformSelector(region)
 
-# Does not verify input for summoner name
-print("Summoner Name?")
-summonerId = input()
+def routingSelector(region):
+    """
 
-queueCode = -1
-# Verify queue input validation
-while queueCode < 0:
-    print("Which queue?")
-    queue = input().lower()
-    queueCode = switch(queue)
+    """
+    if region in ["na", "br", "lan", "las", "oce"]:
+        return "americas"
+    if region in ["kr", "jp"]:
+        return "asia"
+    if region in ["eune", "euw", "tr", "ru"]:
+        return "europe"
+    assert False, "not matched"
 
-print("Loading information from the nickname...")
 
-# Gets start and end time for the function
-start = time.time()
-# Gets list of champions & their winrates as a pair of lists, in order of number of games played
-try:
-    x, y = api.displayWinrates(api.getMatchList(api.getSummonerId(summonerId, region),queueCode, region), region)
-# Exception for when the summonerId request gives an error 
-except:
-    print("That summoner name doesn't exist!")
-    exit()
+if __name__ == "__main__":
+    # Prompt user for inputs
+    region = ""
+    while region not in REGIONS:
+        print("Region? (i.e. NA)")
+        region = input().lower()
 
-end = time.time()
-print(f"Data acquired in {end-start:.2f} seconds.")
+    routing = routingSelector(region)
+    region = REGIONS[region]
 
-fig, ax = plt.subplots()
+    # Does not verify input for summoner name
+    print("Summoner Name?")
+    summonerName = input()
 
-# Plots bar graph
-x_pos = np.arange(len(x))
-bar_plot = plt.bar(x_pos, y)
+    queue = ""
+    # Verify queue input validation
+    while queue not in QUEUES:
+        print("Which queue?")
+        queue = input().lower()
 
-# Set bar colours for different winrates
-for i, bar in enumerate(bar_plot):
-    if y[i]>=70:
-        bar.set_color("#FFAB0F")
-    elif y[i]>=60 and y[i]<70:
-        bar.set_color("#006AB7")
-    elif y[i]>=50 and y[i]<60:
-        bar.set_color("#80C1F0")
+    queueCode = QUEUES[queue]
+
+    print("Loading...")
+
+    # Gets start and end time for the function
+    start = time.time()
+    # Gets list of champions & their winrates as a pair of lists, in order of number of games played
+
+    summonerId = api.getSummonerId(summonerName, region)
+    matchlist = api.getMatchList(summonerId, queueCode, routing)
+    x, y = api.displayWinrates(summonerId, matchlist, routing)
+
+    end = time.time()
+    print(f"Data acquired in {end-start:.2f} seconds.")
+
+    fig, ax = plt.subplots()
+
+    # Plots bar graph
+    x_pos = np.arange(len(x))
+    bar_plot = plt.bar(x_pos, y)
+
+    # Set bar colors for different winrates
+    for i, bar in enumerate(bar_plot):
+        if y[i] >= 70:
+            bar.set_color("#E19205")
+        elif y[i] >= 60 and y[i] < 70:
+            bar.set_color("#1F8ECD")
+        elif y[i] >= 50 and y[i] < 60:
+            bar.set_color("#2DAF7F")
+        else:
+            bar.set_color("grey")
+
+    # Creates labels for each bar
+    for champion, rect in enumerate(bar_plot):
+        # Label in white, inside the bar if the winrate is greater than 90%
+        if y[champion] > 90:
+            height = y[champion] - 5
+            color = 'white'
+        else:
+            height = y[champion] + 0.5
+            color = 'black'
+        text = f"{y[champion]}%"
+        ax.text(
+            rect.get_x() + rect.get_width()/2., height, text,
+            ha='center', va='bottom', rotation=0, fontsize='9', color=color
+        )
+
+    # X and Y axis labels
+    plt.xlabel('Champion')
+    plt.ylabel('Winrate')
+    plt.ylim(0, 100)
+
+    # Rotated X axis label for each champion
+    plt.xticks(
+        x_pos, x,
+        rotation=45, fontsize='10', horizontalalignment='right'
+    )
+
+    # Displays title
+    if queue == 'blind' or queue == 'draft':
+        queue += ' pick'
     else:
-        bar.set_color("grey")
+        queue = 'ranked ' + queue
+    title = f'Winrates per champion for {summonerName} in {queue}'
+    plt.title(title)
 
-# Creates labels for each bar
-for champion,rect in enumerate(bar_plot):
-    # Label in white, inside the bar if the winrate is greater than 90%
-    if y[champion] > 90:
-        height = y[champion] - 5
-        color = 'white'
-    else:
-        height = y[champion] + 0.5
-        color = 'black'
-    text = str(y[champion]) + "%"
-    ax.text(rect.get_x() + rect.get_width()/2., height,
-                text,
-                ha='center', va='bottom', rotation=0, fontsize='9', color=color)
-
-# X and Y axis labels
-plt.xlabel('Champion')
-plt.ylabel('Winrate')
-plt.ylim(0,100)
-
-# Rotated X axis label for each champion
-plt.xticks(x_pos, x, rotation=45, fontsize='10', horizontalalignment='right')
-
-# Displays title
-if queue == 'blind' or queue == 'draft':
-    queue += ' pick'
-else:
-    queue = 'ranked ' + queue
-title = 'Winrates per champion for ' + summonerId + ' in ' + queue
-plt.title(title)
-
-# Prevent labels from being cut off
-plt.tight_layout()
-plt.show()
+    # Prevent labels from being cut off
+    plt.tight_layout()
+    plt.show()
